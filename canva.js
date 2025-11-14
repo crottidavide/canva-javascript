b443b9572fb7b62e3a4ba1fc8ea4da0299f6bc6d
crottidavide <crotti.davide@iisgalvanimi.edu.it>
2025-11-14 09:49:12 +0000

    conclusione

--- canva.js
+++ canva.js
@@ -11,5 +11,18 @@ function buttonFunction() {
    
     ctx.fillRect(40, 40, 10, 10);
 
-}
+    
+    ctx.fillRect(60, 60, 10, 10);
 
+    
+    ctx.fillRect(80, 80, 10, 10);
+
+    
+    ctx.fillRect(100, 100, 10, 10);
+    
+    ctx.fillRect(120, 120, 10, 10);
+    
+    ctx.fillRect(140, 140, 10, 10);
+
+  
+}
