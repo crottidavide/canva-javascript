c7045b58ead37d3bd2a9824f0fd8968f4634f75b
crottidavide <crotti.davide@iisgalvanimi.edu.it>
2025-11-14 09:38:16 +0000

    j

--- canva.js
+++ canva.js
@@ -2,15 +2,14 @@ function buttonFunction() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "#ff9900ff";
-  ctx.fillRect(0, 0, 150, 75);
+  ctx.fillRect(0, 0, 150, 150);
   ctx.fillStyle = "#fbff00ff";
 
   ctx.fillRect(0, 0, 10, 10);
 
   ctx.fillRect(20, 20, 10, 10);
    
-  ctx.fillRect(0, 0, 10, 10);
+    ctx.fillRect(40, 40, 10, 10);
 
-  ctx.fillRect(20, 20, 10, 10);
 }
 
